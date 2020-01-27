e3fcd6033b597d80cdad0b7dc27cf30de5b129fe
Courtney Hay <courtneyhay@student.fullsail.edu>
2020-01-26 21:12:22 -0500

    Created The I am Poor App With my own twist

--- I Am Poor/ViewController.swift
+++ I Am Poor/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lableDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnHelp(_ sender: Any) {
+        lableDisplay.text  = "Help Me"
+    }
+    
 }
 
